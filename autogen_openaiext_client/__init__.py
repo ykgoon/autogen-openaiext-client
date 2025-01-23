dd89d1f1f331f7ae70e737131f484cba5cb59a06
Vaibhav Balloli <balloli.vb@gmail.com>
2025-01-22 23:38:29 -0500

    Fix __init__ and bump release version

--- autogen_openaiext_client/__init__.py
+++ autogen_openaiext_client/__init__.py
@@ -1,10 +1,8 @@
 from autogen_openaiext_client.client import (
-    OpenAIExtChatCompletionClient,
     GeminiChatCompletionClient,
     GroqChatCompletionClient,
     TogetherAIChatCompletionClient,
 )
-from autogen_openaiext_client.config import OpenAIExtClientConfiguration
 from autogen_openaiext_client.info import GeminiInfo, GroqInfo, TogetherAIInfo
 
-__version__ = "0.0.1"
+__version__ = "0.0.3"
